dce94dcac3043d65ee5496e73e749884727bb272
jackfiled <xcrenchangjun@outlook.com>
2022-05-04 16:24:05 +0800

    添加Doxygen注释的示例

--- lib/example.c
+++ lib/example.c
@@ -3,6 +3,11 @@
 //
 #include "example.h"
 
+/**
+ * @brief 在stdin打印一个字符串
+ * 
+ * @param str 指向需要打印的字符串指针
+ */
 void print(char* str)
 {
     printf("%s", str);
--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include "example.h"
 
+/**
+ * @brief 程序的主函数
+ * 
+ * @return int 程序结束的状态，0表示成功
+ */
 int main()
 {
+    /**
+     * @brief 需要输出的字符串
+     * 
+     */
     char* string = "Good morning!\n";
     print(string);
     return 0;
