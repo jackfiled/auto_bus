a044ff104489d70dedf0a0a0e9102ba4b5a6e9f0
jackfiled <xcrenchangjun@outlook.com>
2022-06-26 12:09:19 +0800

    完善了请求列表的相关槽函数

--- src/centralwidget.cpp
+++ src/centralwidget.cpp
@@ -15,10 +15,66 @@ CentralWidget::CentralWidget(QWidget *parent) : QWidget(parent), ui(new Ui::Cent
     scene_manager = new SceneManager(20);
 
     ui->main_canva->setScene(scene_manager->scene);
+
+    SetupQueryList();
 }
 
 CentralWidget::~CentralWidget()
 {
+    DeleteQueryList();
+
     delete scene_manager;
     delete ui;
+}
+
+void CentralWidget::SetupQueryList()
+{
+    // 设置请求列表的表头
+    QueryListItem *column_name = new QueryListItem(1,1);
+    column_name->SetColumnName();
+
+    query_items.push_back(column_name);
+
+    // 在列表中显示
+    QListWidgetItem *widget_item = new QListWidgetItem;
+    ui->query_list->addItem(widget_item);
+    ui->query_list->setItemWidget(widget_item, column_name);
+}
+
+void CentralWidget::DeleteQueryList()
+{
+    for(auto itor = query_items.begin(); itor != query_items.end(); ++itor)
+    {
+        delete *itor;
+    }
+    query_items.clear();
+}
+
+void CentralWidget::AppendQueryItem(int query_type, int node_id)
+{
+    QueryListItem *item = new QueryListItem(query_type, node_id);
+
+    query_items.push_back(item);
+
+    // 在列表中显示
+    QListWidgetItem *widget_item = new QListWidgetItem;
+    ui->query_list->addItem(widget_item);
+    ui->query_list->setItemWidget(widget_item, item);
+}
+
+void CentralWidget::DeleteQueryItem(int query_id)
+{
+    // 由于表头的存在，且请求的编号从1开始，请求的编号恰好就是请求在列表中的位置
+    QListWidgetItem *deleted_widget = ui->query_list->takeItem(query_id);
+
+    auto itor = query_items.begin();
+
+    for(int i = 0; i < query_id; i++)
+    {
+        ++itor;
+    }
+
+    delete deleted_widget;
+    delete *itor;
+    query_items.erase(itor);
 }
--- src/header/centralwidget.h
+++ src/header/centralwidget.h
@@ -6,8 +6,12 @@
 #define AUTO_BUS_GUI_CENTRAL_WIDGET_H
 
 #include <QWidget>
+#include "QListWidgetItem"
 #include "string"
+#include "list"
+
 #include "mainScene.h"
+#include "queryListItem.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui
@@ -26,11 +30,41 @@ public:
     ~CentralWidget() override;
 
 public slots:
+    /**
+     * 处理添加一个请求的槽函数
+     * @param query_type 请求的类型
+     * @param node_id 请求指向的站点id
+     */
+    void AppendQueryItem(int query_type, int node_id);
 
+    void DeleteQueryItem(int query_id);
 
 private:
+    /**
+     * UI控件
+     */
     Ui::CentralWidget *ui;
+
+    /**
+     * 画图的场景管理器
+     */
     SceneManager *scene_manager;
+
+    /**
+     * 请求列表中的对象
+     */
+    std::list<QueryListItem *> query_items;
+
+    /**
+     * 初始化请求展示列表
+     * 展示表头的说明文字
+     */
+    void SetupQueryList();
+
+    /**
+     * 清除展示对象占用的内存空间
+     */
+    void DeleteQueryList();
 };
 
 
